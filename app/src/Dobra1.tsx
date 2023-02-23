254346e791f14e84fa04f99885ac9dca6109ddb9
Bruno Juliao <juliaob@curtisinst.com>
2023-02-23 11:11:47 -0300

    Acertando tamanho de volta

--- app/src/Dobra1.tsx
+++ app/src/Dobra1.tsx
@@ -4,7 +4,7 @@ import photo_banner from "./photo_banner.png"
 function Dobra1() {
   return (
     <>
-      <h3 className='text-xs text-center font-bold uppercase'>Antecipação do saque aniversário do FGTS</h3>
+      <h3 className='text-[10px] text-center font-bold uppercase'>Antecipação do saque aniversário do FGTS</h3>
       <h2 className="text-lg text-center font-bold my-1">Antecipe até <span className="text-[#D5B473]">10 Anos</span> do seu FGTS</h2>
       <div className="text-xs text-center w-[297px] mx-auto my-2">Adiante até 10 parcelas do seu Saque Aniversário. Clique em fale conosco, nos chame no Whatsapp, e receba seu adiantamento hoje mesmo.</div>
       <button className="bg-[#158B7D] text-white rounded-xl px-4 py-2 flex justify-evenly w-44 items-center mx-auto mt-5"><p className="text-xl">Fale conosco</p><img className="w-5 h-5" src={wp} alt="Logo do WhatsApp"/></button>
