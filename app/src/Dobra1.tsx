508b5530b92212edc5ea7f581896dbbb1280b83a
Bruno Juliao <juliaob@curtisinst.com>
2023-02-23 01:21:27 -0300

    Dobra 1

---
+++ app/src/Dobra1.tsx
@@ -0,0 +1,16 @@
+import wp from "./wp.png";
+import photo_banner from "./photo_banner.png"
+
+function Dobra1() {
+  return (
+    <>
+      <h1 className='text-[10px] text-center font-bold'>Antecipação do saque aniversário do FGTS</h1>
+      <h2 className="text-lg text-center font-bold my-1">Antecipe até <span className="text-[#D5B473]">10 Anos</span> do seu FGTS</h2>
+      <div className="text-xs text-center w-[297px] mx-auto my-2">Adiante até 10 parcelas do seu Saque Aniversário. Clique em fale conosco, nos chame no Whatsapp, e receba seu adiantamento hoje mesmo.</div>
+      <button className="bg-[#158B7D] text-white rounded-xl px-4 py-2 flex justify-evenly w-44 items-center mx-auto my-5"><p className="text-xl">Fale conosco</p><img className="w-5 h-5" src={wp}/></button>
+      <img className="mx-auto w-60" src={photo_banner}/>
+    </>
+  );
+}
+
+export default Dobra1;
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}',],
+  content: ['./src/**/*.{js,jsx,ts,tsx}','./public/index.html'],
   theme: {
+    screens: {
+      'tablet': '481px',
+      'pc': '769px',
+    },
     extend: {},
   },
   plugins: [],
