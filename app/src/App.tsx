36c5461f7bcf5ff94c92d4f33779226d05767ebb
Bruno Juliao <juliaob@curtisinst.com>
2023-02-26 21:51:51 -0300

    Dobra 4

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import Dobra1 from './dobra1/Dobra1';
 import Dobra2 from './dobra2/Dobra2';
 import Dobra3 from './dobra3/Dobra3';
+import Dobra4 from './dobra4/Dobra4';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Dobra1/>
       <Dobra2/>
       <Dobra3/>
+      <Dobra4/>
     </>
   );
 }
---
+++ app/src/dobra4/Dobra4.tsx
@@ -0,0 +1,36 @@
+import n1 from './num1.svg';
+import n2 from './num2.svg';
+import n3 from './num3.svg';
+import n4 from './num4.svg';
+
+function Dobra4() {
+  return(
+    <>
+      <h2 className="text-[33px] text-center font-bold">Nossos <span className="text-[#D5B473]">números</span></h2>
+      <div className="flex flex-wrap justify-center gap-y-12 gap-x-7 my-10">
+        <div className="num w-[158px] h-[132px] flex flex-col pt-5 items-center text-center">
+          <img className='mb-1' src={n1} alt="Fita de premiação"/>
+          <h4 className='text-xl font-bold'>+35 mil</h4>
+          <div className='text-base'>clientes atendidos</div>
+        </div>
+        <div className="num w-[158px] h-[132px] flex flex-col pt-5 items-center text-center">
+          <img className='mb-2' src={n2} alt="Medalha"/>
+          <h4 className='text-base font-bold'>Desde 2014</h4>
+          <div className='text-base'>no mercado</div>
+        </div>
+        <div className="num w-[158px] h-[132px] flex flex-col pt-5 items-center text-center">
+          <img className='mb-3' src={n3} alt="Grupo"/>
+          <h4 className='text-base font-bold'>+ de 50 milhões</h4>
+          <div className='text-base'>de crédito liberado</div>
+        </div>
+        <div className="num w-[158px] h-[132px] flex flex-col pt-5 items-center text-center">
+          <img className='mb-2' src={n4} alt="Estrela"/>
+          <h4 className='text-xl font-bold mb-1'>Nota 4.9</h4>
+          <div className='text-xs'>De avaliação no google</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Dobra4;
