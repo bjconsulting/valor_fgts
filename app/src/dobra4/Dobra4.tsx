ab5a7592766b6f30f93de1f953be0f3f5ad1a1a3
Bruno Juliao <juliaob@curtisinst.com>
2023-02-26 21:54:10 -0300

    Acerto na distância vertical na Dobra 4

--- app/src/dobra4/Dobra4.tsx
+++ app/src/dobra4/Dobra4.tsx
@@ -7,7 +7,7 @@ function Dobra4() {
   return(
     <>
       <h2 className="text-[33px] text-center font-bold">Nossos <span className="text-[#D5B473]">números</span></h2>
-      <div className="flex flex-wrap justify-center gap-y-12 gap-x-7 my-10">
+      <div className="flex flex-wrap justify-center gap-y-12 gap-x-7 my-12">
         <div className="num w-[158px] h-[132px] flex flex-col pt-5 items-center text-center">
           <img className='mb-1' src={n1} alt="Fita de premiação"/>
           <h4 className='text-xl font-bold'>+35 mil</h4>
