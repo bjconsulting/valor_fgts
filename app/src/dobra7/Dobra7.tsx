5405cdf4193959729c5838d55413cfec9bcfa272
Bruno Juliao <juliaob@curtisinst.com>
2023-03-01 00:53:08 -0300

    Dobra 7

--- app/src/dobra7/Dobra7.tsx
+++ app/src/dobra7/Dobra7.tsx
@@ -1,8 +1,8 @@
 function Dobra7() {
   return(
     <div className="my-9">
-      <h2 className="text-xl text-center mb-5 font-bold">Quem somos nós</h2>
-      <iframe className="w-[327px] h-[174px] mx-auto" src="https://www.youtube.com/embed/6Q9mLiNtzTA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      <h2 className="text-xl text-center mb-5 font-bold tablet:text-[49px]">Quem somos nós</h2>
+      <iframe className="w-[327px] h-[174px] mx-auto tablet:mt-9 tablet:w-[700px] tablet:h-[300px]" src="https://www.youtube.com/embed/6Q9mLiNtzTA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
     </div>
   )
 }
