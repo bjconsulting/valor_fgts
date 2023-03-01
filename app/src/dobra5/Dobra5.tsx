60f73311ea4b1724faa584350c0a080c80ceabd6
Bruno Juliao <juliaob@curtisinst.com>
2023-03-01 00:42:33 -0300

    Dobra 5 e correção header

--- app/src/dobra5/Dobra5.tsx
+++ app/src/dobra5/Dobra5.tsx
@@ -5,40 +5,42 @@ import modelo1 from './modelo1.png'
 function Dobra5() {
   return(
     <>
-      <div className="bg-[#313F55] text-white text-center pt-4">
-        <h2 className="text-xl font-bold mb-8"><span className="text-[#D5B473]">Como obter</span> meu empréstimo</h2>
-        <div className='w-64 mx-auto text-left flex flex-col gap-y-2'>
+      <div className="bg-[#313F55] text-white text-center pt-4 tablet:pt-16 tablet:mt-20">
+        <h2 className="text-xl font-bold mb-8 tablet:text-[52px]"><span className="text-[#D5B473]">Como obter</span> meu empréstimo</h2>
+        <div className='w-64 mx-auto text-left flex flex-col gap-y-2 tablet:w-[620px] tablet:gap-5 tablet:mt-16'>
           <div className='text-xs flex flex-row gap-x-2 items-start'>
-            <img className='mt-1 w-[12px] h-[8px]' src={check} alt="" />
-            <span>Ser maior de 18 anos ou emancipado;</span>
+            <img className='mt-1 w-[12px] h-[8px] tablet:w-[24.5px] tablet:h-[15.41px]' src={check} alt="" />
+            <span className='tablet:text-[20px]'>Ser maior de 18 anos ou emancipado;</span>
           </div>
           <div className='text-xs flex flex-row gap-x-2 items-start'>
-            <img className='mt-1 w-[12px] h-[8px]' src={check} alt="" />
-            <span>CPF em situação regular na receita;</span>
+            <img className='mt-1 w-[12px] h-[8px] tablet:w-[24.5px] tablet:h-[15.41px]' src={check} alt="" />
+            <span className='tablet:text-[20px]'>CPF em situação regular na receita;</span>
           </div>
           <div className='text-xs flex flex-row gap-x-2 items-start'>
-            <img className='mt-1 w-[12px] h-[8px]' src={check} alt="" />
-            <span>Ter conta-corrente ou poupança</span>
+            <img className='mt-1 w-[12px] h-[8px] tablet:w-[24.5px] tablet:h-[15.41px]' src={check} alt="" />
+            <span className='tablet:text-[20px]'>Ter conta-corrente ou poupança</span>
           </div>
           <div className='text-xs flex flex-row gap-x-2 items-start'>
-            <img className='mt-1 w-[12px] h-[8px]' src={check} alt="" />
-            <span>Optar pelo saque de aniversário</span>
+            <img className='mt-1 w-[12px] h-[8px] tablet:w-[24.5px] tablet:h-[15.41px]' src={check} alt="" />
+            <span className='tablet:text-[20px]'>Optar pelo saque de aniversário</span>
           </div>
           <div className='text-xs flex flex-row gap-x-2 items-start'>
-            <img className='mt-1 w-[12px] h-[8px]' src={check} alt="" />
-            <span>Autorizar o banco a acessar seus dados e valores do FGTS;</span>
+            <img className='mt-1 w-[12px] h-[8px] tablet:w-[24.5px] tablet:h-[15.41px]' src={check} alt="" />
+            <span className='tablet:text-[20px]'>Autorizar o banco a acessar seus dados e valores do FGTS;</span>
           </div>
           <div className='text-xs flex flex-row gap-x-2 items-start'>
-            <img className='mt-1 w-[12px] h-[8px]' src={check} alt="" />
-            <span>Aprovar a simulação</span>
+            <img className='mt-1 w-[12px] h-[8px] tablet:w-[24.5px] tablet:h-[15.41px]' src={check} alt="" />
+            <span className='tablet:text-[20px]'>Aprovar a simulação</span>
           </div>
         </div>
 
-        <h2 className="text-xl font-bold mt-8 text-[#D5B473]">Você atende todos os requisitos?</h2>
+        <div className='tablet:relative tablet:h-[476px] tablet:mt-[50px]'>
+          <h2 className="text-xl font-bold mt-8 text-[#D5B473] tablet:text-[32px] tablet:absolute tablet:top-[81px] tablet:right-[5px]">Você atende todos os requisitos?</h2>
 
-        <button className="bg-[#158B7D] text-white rounded-[14px] px-4 py-2 flex justify-evenly w-[125px] items-center mx-auto my-4"><p className="text-xs">Fale conosco</p><img className="w-[14px] h-[14px]" src={wp} alt="Logo do WhatsApp"/></button>
+          <button className="bg-[#158B7D] text-white rounded-xl px-4 py-2 flex justify-evenly w-44 items-center mx-auto mt-5 tablet:absolute tablet:top-[250px] tablet:right-[160px] tablet:w-[198px] tablet:h-[64px]"><p className="text-xl">Fale conosco</p><img className="w-5 h-5" src={wp} alt="Logo do WhatsApp"/></button>
 
-        <img className='w-72 mx-auto' src={modelo1} alt="Pessoa sorrindo olhando para o celular"/>
+          <img className='w-72 mx-auto tablet:absolute tablet:top-0 tablet:w-[498px] tablet:h-auto tablet:left-[-50px]' src={modelo1} alt="Pessoa sorrindo olhando para o celular"/>
+        </div>
       </div>
     </>
   )
--- app/src/header/Header.tsx
+++ app/src/header/Header.tsx
@@ -6,7 +6,7 @@ import logo from './logo.png'
 function Header() {
     const [open, setOpen] = useState(false);
     return (
-        <header className="max-w-[393px] py-8 mx-auto tablet:w-[834px] tablet:mx-auto tablet:max-w-[834px]">
+        <header className="max-w-[393px] py-8 mx-auto tablet:mx-auto tablet:max-w-[834px]">
             <div className="max-w-[90%] mx-auto flex justify-between">
                 <picture className="pointer-events-none">
                     <img className="w-[82px] tablet:w-[168px]" src={logo} alt="Valor Serviços" />
