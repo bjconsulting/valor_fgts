b3462cee89b504ab5faf9288f79665894e45f2b5
Bruno Juliao <juliaob@curtisinst.com>
2023-03-23 11:15:22 -0300

    Pequenos ajustes

--- app/src/TrackLead.tsx
+++ app/src/TrackLead.tsx
@@ -2,19 +2,18 @@ declare const fbq: (trackType: string, options?: any) => boolean;
 declare const gtag_report_conversion: () => boolean;
 
 export default function TrackLead() {
-  console.log({fbq})
+  console.info('Track Lead Click', Date.now())
   if (fbq) {
     fbq('track', 'Lead')
-    console.log('I know fbq and track was called')
+    console.info('I know fbq and track was called')
   } else {
-    console.log('I don\'t know fbq')
+    console.info('I don\'t know fbq')
   }
 
-  console.log({gtag_report_conversion})
   if (gtag_report_conversion) {
     gtag_report_conversion()
-    console.log('I know gtag_report_conversion and used it')
+    console.info('I know gtag_report_conversion and used it')
   } else {
-    console.log('I don\'t know gtag_report_conversion')
+    console.info('I don\'t know gtag_report_conversion')
   }
 }
--- app/src/WhatsAppCta.tsx
+++ app/src/WhatsAppCta.tsx
@@ -9,6 +9,6 @@ type WhatsAppCtaProps = {
 
 export default function WhatsAppCta(props: WhatsAppCtaProps) {
   return(
-    <a href={WhatsAppCtaUrl()} target="_blank" rel="noreferrer" className={props.buttonClassName}><p className={props.textClassName} onClick={() => TrackLead() }>Fale conosco</p><RiWhatsappLine className="w-5 h-5" /></a>
+    <a href={WhatsAppCtaUrl()} target="_blank" rel="noreferrer" className={props.buttonClassName} onClickCapture={(e) => TrackLead() }><p className={props.textClassName}>Fale conosco</p><RiWhatsappLine className="w-5 h-5" /></a>
   )
 }
